fe3f06f36e3def71408f63243b4afcfe156f8157
waleedahmd308 <f190953@nu.edu.pk>
2023-04-26 23:41:05 +0500

    updateVersion of the code

--- Flutter-Onboarding-Screen-master/README.md
+++
@@ -1,6 +0,0 @@
-# Flutter Onboarding Screen
-
-### Onboarding Screen UI
-
-![App UI](/UI.png)
-
--- Flutter-Onboarding-Screen-master/android/app/src/main/kotlin/com/example/onboarding_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.onboarding_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
